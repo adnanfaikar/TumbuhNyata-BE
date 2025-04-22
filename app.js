9a0ae7a851b742f926a5eb0ea065070fe117abe2
adnanfaikar <adnan.faika@gmail.com>
2025-04-22 11:34:38 +0700

    memperbaiki urutan penggunaan middleware dan memastikan rute otentikasi terdaftar dengan benar

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ const authRoutes = require('./routes/authRoutes');
 const app = express();
 
 app.use(cors()); 
-app.use(express.json()); // parsing req.body dalam format JSON
-app.use(express.urlencoded({ extended: true })); // parsing data URL-encoded
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); 
 
 const PORT = process.env.PORT || 5000;
-app.use("/auth", authRoutes);
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
+app.use("/auth", authRoutes);
